f5da312db18f55123efef1fd4089c10243e53282
!KLtrNEnf1AI76y1 <alextogus@gmail.com>
2020-03-28 23:28:02 +0100

    French transalation by alexetgus

---
+++ Themes/default/languages/Redirector/Redirector.french-utf8.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package SMF Redirector
+ * @file Redirector.english-utf8.php
+ * @author digger <digger@mysmf.ru> <http://mysmf.ru>
+ * @copyright Copyright (c) 2015-2017, digger
+ * @license The MIT License (MIT) https://opensource.org/licenses/MIT
+ * @French transtation, alexetgus, http://chez-oim.org
+ * @version 1.2
+ */
+
+$txt['redirector_description'] = 'Rediriger et cacher les liens externes. Vous devez ajouter à votre fichier robots.txt<br> 
+Disallow: /*action=go;';
+$txt['redirector_admin_menu'] = 'Redirecteur de liens';
+$txt['redirector_enabled'] = 'Permettre la redirection';
+$txt['redirector_guest_only'] = 'Pour invités seulement';
+$txt['redirector_check_referer'] = 'Rediriger vers la page principale pour les références vides ou étrangères';
+$txt['redirector_mode'] = 'Mode de redirection';
+$txt['redirector_mode_immediate'] = 'instantané';
+$txt['redirector_mode_delayed'] = 'retardé';
+$txt['redirector_delay'] = 'Délai en secondes';
+$txt['redirector_whitelist'] = 'Liste blanche de domaines';
+$txt['redirector_whitelist_sub'] = 'Un domaine par ligne';
+$txt['redirector_page_title'] = 'Rediriger vers un lien externe';
+$txt['redirector_hide_links_title'] = 'Cacher les liens';
+$txt['redirector_hide_guest_links'] = 'Cacher les liens pour les invités (sauf liste blanche)';
+$txt['redirector_hide_guest_message'] = '[Connexion ou inscription]';
+$txt['redirector_hide_guest_custom_message'] = 'Message à afficher lorsque le lien est caché';
+$txt['whoall_go'] = 'Suivre le lien externe';
+$txt['redirector_nofollow_links'] = 'Ajouter rel="nofollow noopener" sur les liens';
+$txt['redirector_page_text'] = 'Vous allez suivre un lien externe. Vous serez redirigé dans %d sec.';
---
+++ Themes/default/languages/Redirector/Redirector.french.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package SMF Redirector
+ * @file Redirector.english-utf8.php
+ * @author digger <digger@mysmf.ru> <http://mysmf.ru>
+ * @copyright Copyright (c) 2015-2017, digger
+ * @license The MIT License (MIT) https://opensource.org/licenses/MIT
+ * @French transtation, alexetgus, http://chez-oim.org
+ * @version 1.2
+ */
+
+$txt['redirector_description'] = 'Rediriger et cacher les liens externes. Vous devez ajouter &agrave; votre fichier robots.txt<br> 
+Disallow: /*action=go;';
+$txt['redirector_admin_menu'] = 'Redirecteur de liens';
+$txt['redirector_enabled'] = 'Permettre la redirection';
+$txt['redirector_guest_only'] = 'Pour invit&eacute;s seulement';
+$txt['redirector_check_referer'] = 'Rediriger vers la page principale pour les r&eacute;f&eacute;rences vides ou &eacute;trang&egrave;res';
+$txt['redirector_mode'] = 'Mode de redirection';
+$txt['redirector_mode_immediate'] = 'instantan&eacute;';
+$txt['redirector_mode_delayed'] = 'retard&eacute;';
+$txt['redirector_delay'] = 'D&eacute;lai en secondes';
+$txt['redirector_whitelist'] = 'Liste blanche de domaines';
+$txt['redirector_whitelist_sub'] = 'Un domaine par ligne';
+$txt['redirector_page_title'] = 'Rediriger vers un lien externe';
+$txt['redirector_hide_links_title'] = 'Cacher les liens';
+$txt['redirector_hide_guest_links'] = 'Cacher les liens pour les invit&eacute;s (sauf liste blanche)';
+$txt['redirector_hide_guest_message'] = '[Connexion ou inscription]';
+$txt['redirector_hide_guest_custom_message'] = 'Message &agrave; afficher lorsque le lien est cach&eacute;';
+$txt['whoall_go'] = 'Suivre le lien externe';
+$txt['redirector_nofollow_links'] = 'Ajouter rel="nofollow noopener" sur les liens';
+$txt['redirector_page_text'] = 'Vous allez suivre un lien externe. Vous serez redirig&eacute; dans %d sec.';
